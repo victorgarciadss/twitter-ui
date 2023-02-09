f34bb9b2fb2e4ee4c1fef14c2584a5e57dc4e1a1
victorgarciadss <garciavictor.1308@gmail.com>
2023-02-09 17:26:14 -0300

    feat: adding responses and events

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,33 +1,61 @@
+import { FormEvent, KeyboardEvent, useState } from "react"
 import { HeaderTimeline } from "../components/HeaderTimeline"
 import { Tweet } from "../components/Tweet"
 import { StatusStyle } from "../styles/StatusStyle"
 
-const answers = [
-    "Concordo...",
-    "Olha, faz sentido!",
-    "Belo comentário"
-]
+
 
 export const Status = () => {
+
+    const [newAnswer, setNewAnswer] = useState<string>('');
+    const [answers, setAnswers] = useState([
+        "Concordo...",
+        "Olha, faz sentido!",
+        "Belo comentário"
+    ])
+
+    const createNewAnswer = (e: FormEvent) => {
+        e.preventDefault();
+
+        setAnswers([newAnswer, ...answers]);
+
+        setNewAnswer("");
+    }
+
+    const handleHotKeySubmit = (e: KeyboardEvent) => {
+        if (e.key === "Enter" && e.ctrlKey) {
+            setAnswers([newAnswer, ...answers]);
+            setNewAnswer("");
+        }
+    }
+
     return (
         <StatusStyle>
             <HeaderTimeline title='Sequência' />
 
 
-            <Tweet content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quas nostrum ipsum atque aperiam omnis, pariatur ab praesentium voluptate qui dolorem, sequi officiis quis explicabo veritatis libero minus expedita? Earum.'/>
+            <Tweet content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quas nostrum ipsum atque aperiam omnis, pariatur ab praesentium voluptate qui dolorem, sequi officiis quis explicabo veritatis libero minus expedita? Earum.' />
 
             <div className='separator' />
 
-            <form className='answer-tweet-form' action="">
+            <form onSubmit={createNewAnswer} className='answer-tweet-form' action="">
                 <label htmlFor="tweet">
                     <img src="https://github.com/victorgarciadss.png" alt="Imagem Victor" />
-                    <textarea id="tweet" placeholder='Tweete sua resposta' />
+                    <textarea
+                        id="tweet"
+                        placeholder='Tweete sua resposta'
+                        value={newAnswer}
+                        onKeyDown={handleHotKeySubmit}
+                        onChange={(event) => {
+                            setNewAnswer(event.target.value);
+                        }}
+                    />
                 </label>
 
                 <button type='submit'>Responder</button>
             </form>
 
-            
+
 
             {answers.map(answer => {
                 return <Tweet key={answer} content={answer} />
--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -3,25 +3,54 @@ import { TimelineStyle } from '../styles/TimelineStyle';
 import { HeaderTimeline } from '../components/HeaderTimeline';
 
 import { Tweet } from '../components/Tweet';
+import { FormEvent, KeyboardEvent, useState } from 'react';
 
-const tweets = [
-    "Meu primeiro Tweet",
-    "Legal esse lugar",
-    "Show"
-]
+
 
 
 export const Timeline = () => {
+
+  const [newTweet, setNewTweet] = useState<string>('');
+  const [tweets, setTweets] = useState([
+    "Meu primeiro Tweet",
+    "Legal esse lugar",
+    "Show"
+  ])
+
+  const createNewTweet = (e: FormEvent) => {
+    e.preventDefault();
+
+    setTweets([newTweet, ...tweets]);
+
+    setNewTweet("");
+  }
+
+  const handleHotKeySubmit = (e: KeyboardEvent) => {
+    if (e.key === "Enter" && e.ctrlKey) {
+      setTweets([newTweet, ...tweets]);
+      setNewTweet("");
+    }
+}
+
+
     return(
         <TimelineStyle>
             <HeaderTimeline title='Página Inicial'/>
 
             
             
-            <form className='new-tweet-form' action="">
+            <form onSubmit={createNewTweet} className='new-tweet-form' action="">
               <label htmlFor="tweet">
                 <img src="https://github.com/victorgarciadss.png" alt="Imagem Victor" />
-                <textarea id="tweet" placeholder='O que está acontencendo?' />
+                <textarea
+                  id="tweet"
+                  placeholder='O que está acontencendo?'
+                  value={newTweet}
+                  onKeyDown={handleHotKeySubmit}
+                  onChange={(event) => {
+                    setNewTweet(event.target.value);
+                  }}
+                />
               </label>
 
               <button type='submit'>Tweetar</button>
